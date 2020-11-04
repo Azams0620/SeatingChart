1423ffbb24dc54d15c3f693542b292899f3b4411
Azams0620 <sayeed.azam@patelhs.org>
2020-11-04 17:21:33 +0000

    Created blank array for student names.

--- Main.java
+++ Main.java
@@ -3,6 +3,9 @@ class Main {
     // Array of integers for seats
     int [] seats = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
 
+    // Array of strings for student names
+    String [] studentNames = new String[15];
+
     
 
 
